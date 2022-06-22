d982ba062c8db4a378491fa06c1c2e10bf8149a2
Baron Drakula <karyol.kj@gmail.com>
2022-06-22 20:08:51 +0200

    Some sketch

--- ts/src/in_game/in_game.ts
+++ ts/src/in_game/in_game.ts
@@ -5,7 +5,7 @@ import {
 } from "@overwolf/overwolf-api-ts";
 
 import { AppWindow } from "../AppWindow";
-import { kHotkeys, kWindowNames, kGamesFeatures } from "../consts";
+import { kHotkeys, kWindowNames, kGamesFeatures, kERCharacterIDS, kERWeaponIDS, kERCharacterWeapons } from "../consts";
 
 import WindowState = overwolf.windows.WindowStateEx;
 
@@ -19,12 +19,18 @@ class InGame extends AppWindow {
   private _gameEventsListener: OWGamesEvents;
   private _eventsLog: HTMLElement;
   private _infoLog: HTMLElement;
+  private _mainContainer: HTMLElement;
+  private _selectedCharacter: HTMLElement;
+  private _selectedCharacterID: number;
+  private _selectedCharacterName: string;
 
   private constructor() {
     super(kWindowNames.inGame);
 
     this._eventsLog = document.getElementById('eventsLog');
     this._infoLog = document.getElementById('infoLog');
+    this._mainContainer = document.getElementById('other');
+    this._selectedCharacter = document.getElementById('selectedCharacter');
 
     this.setToggleHotkeyBehavior();
     this.setToggleHotkeyText();
@@ -64,20 +70,52 @@ class InGame extends AppWindow {
   private onNewEvents(e) {
     const shouldHighlight = e.events.some(event => {
       switch (event.name) {
-        case 'kill':
-        case 'death':
-        case 'assist':
-        case 'level':
-        case 'matchStart':
-        case 'match_start':
-        case 'matchEnd':
-        case 'match_end':
+        case 'select_character':
+        case 'select_weapon':
+        case 'select_trait':
           return true;
       }
 
       return false
     });
     this.logLine(this._eventsLog, e, shouldHighlight);
+
+    switch (e.events[0].name) {
+      case "select_character":
+        this._selectedCharacter.innerHTML = '';
+        const divInline = document.createElement('div');
+        divInline.className = "inline";
+
+        const selectedCharacter = document.createElement('h1');
+        this._selectedCharacterID = parseInt(e.events[0].data);
+        this._selectedCharacterName = kERCharacterIDS[parseInt(e.events[0].data) - 1];
+        selectedCharacter.textContent = this._selectedCharacterName;
+
+        const characterImage = document.createElement('img');
+        characterImage.src = "https://cdn.dak.gg/er/game-assets/0.60.0/character/CharCommunity_" + this._selectedCharacterName + "_S000.png"
+
+        const selectedWeapon = document.createElement('div');
+        selectedWeapon.id = "selectedWeapon";
+
+        divInline.appendChild(characterImage);
+        divInline.appendChild(selectedCharacter);
+        divInline.appendChild(selectedWeapon);
+        this._selectedCharacter.appendChild(divInline);
+        break;
+
+      case "select_weapon":
+        const characterWeapon = kERCharacterWeapons[this._selectedCharacterID - 1];
+        const weaponName = kERWeaponIDS[characterWeapon[e.events[0].data.substr(this._selectedCharacterID.toString().length) - 1]];
+        document.getElementById('selectedWeapon').textContent = weaponName;
+        const url = "https://dak.gg/bser/characters/" + this._selectedCharacterName + "?teamMode=SOLO&weaponType=" + weaponName;
+        break;
+      
+      default:
+        // document.getElementById('selectedWeapon').textContent = JSON.stringify(e.events[0]);
+        break;
+    }
+
+    
   }
 
   // Displays the toggle minimize/restore hotkey in the window header
@@ -112,6 +150,32 @@ class InGame extends AppWindow {
   private logLine(log: HTMLElement, data, highlight) {
     const line = document.createElement('pre');
     line.textContent = JSON.stringify(data);
+
+    // if (log == this._eventsLog) {
+    //   switch (data.events[0].name) {
+    //     case "select_character":
+    //       line.textContent = JSON.stringify(data);
+    //       break;
+    //     case "matching_start":
+    //       var gameMode = data.events[0].data.split(',')[1].split(':')[1];
+    //       gameMode = gameMode.substr(1, gameMode.length - 3);
+    //       if (gameMode == "cobalt") {
+    //         line.textContent = "cobalt";
+    //         console.log("cobalt");
+    //       } else {
+    //         line.textContent = JSON.stringify(data);
+    //       }
+    //       break;
+    //     default:
+    //       line.textContent = JSON.stringify(data);
+    //       break;
+    //   }
+    // }
+
+    if (log == this._eventsLog && data.events[0].name == "select_weapon") {
+      line.textContent = kERCharacterWeapons[this._selectedCharacterID - 1].toString();
+      // line.textContent = "https://dak.gg/bser/characters/" + this._selectedCharacterName + "?teamMode=SOLO&weaponType=" + kERWeaponIDS[parseInt(data.events[0].data)];
+    }
 
     if (highlight) {
       line.className = 'highlight';
